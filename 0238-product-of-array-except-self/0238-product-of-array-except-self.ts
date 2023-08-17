cc845b9ba19669983cd142e45a2fba9241a170e9
Mohaiminul Eraj <eraj471@gmaIl.com>
2023-08-17 19:31:02 +0600

    Time: 88 ms (88.82%), Space: 56.4 MB (23.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,25 +1,15 @@
 function productExceptSelf(nums: number[]): number[] {
-    const prefix:number[] = []
-    const postfix:number[] = []
-    const res:number[] = []
-    prefix.push(nums[0])
-    postfix.push(nums[nums.length - 1])
-    for(let i = 1; i < nums.length; i++){
-        prefix.push(prefix[i-1] * nums[i])
+    let prefix = 1
+    let postfix = 1
+    const res:number[] = [1 * nums.length]
+    for(let i = 0; i < nums.length; i++){
+        res[i] = prefix;
+        prefix *= nums[i]
     }
-    for(let i=nums.length - 2; i > 0; i--){
-        postfix.unshift(postfix[0] * nums[i])
+    for(let i=nums.length - 1; i >= 0; i--){
+        res[i] *= postfix
+        postfix *= nums[i]
     }
-    postfix.unshift(postfix[0]*nums[0])
-    for(let i=0; i<nums.length; i++){
-        if(i-1 < 0){
-            res.push(postfix[i+1])
-        }
-        else if(i+1 > nums.length - 1){
-            res.push(prefix[i-1])
-        } else {
-            res.push(prefix[i-1]*postfix[i+1])
-        }
-    }
+    
     return res
 };
