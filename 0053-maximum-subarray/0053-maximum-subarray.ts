e9c653b4634beb609c3e8ace82020d722336e02d
Mohaiminul Eraj <eraj471@gmaIl.com>
2023-08-17 13:29:06 +0600

    Time: 67 ms (95.19%), Space: 51.9 MB (83.23%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -0,0 +1,9 @@
+function maxSubArray(nums: number[]): number {
+    let sum = nums[0];
+    let maxSum = nums[0];
+    for(let i = 1; i< nums.length; i++){
+        sum = Math.max(sum + nums[i], nums[i])
+        maxSum = Math.max(maxSum, sum)
+    }
+    return maxSum;
+};
