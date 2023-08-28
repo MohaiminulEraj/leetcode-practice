b78cc814ce6139c597729c5ee393d5d785e24a16
Mohaiminul Eraj <eraj471@gmaIl.com>
2023-08-28 17:43:52 +0600

    Time: 0 ms (100%), Space: 7.3 MB (84.77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,6 @@
+bool isAlphaNum(char c);    // Function Prototyping
 class Solution {
 public:
-    bool isAlphaNum(char c){
-        int ascii = (int)c;
-        if(
-            int('A') <= ascii && int('Z') >= ascii
-            ||
-            int('a') <= ascii && int('z') >= ascii
-            ||
-            int('0') <= ascii && int('9') >= ascii
-        ){
-            return true;
-        }
-        return false;
-    }
-
     bool isPalindrome(string s) {
         int left = 0, right = s.length() - 1;
         while(left < right){
@@ -31,5 +18,19 @@ public:
             right--;
         }
         return true;
+    }
+
+    bool isAlphaNum(char c){
+        int ascii = (int)c;
+        if(
+            int('A') <= ascii && int('Z') >= ascii
+            ||
+            int('a') <= ascii && int('z') >= ascii
+            ||
+            int('0') <= ascii && int('9') >= ascii
+        ){
+            return true;
+        }
+        return false;
     }
 };
